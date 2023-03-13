03d6c5850b09da5755fca8692fb10ccd714381e0
NadeGolubeva <colombe466@gmail.com>
2023-03-13 12:26:56 +0200

    hw10

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,6 +1,17 @@
 export const fetchCountries = (name) => {
   const url = `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,languages,flags`;
   return fetch(url)
-    .then((response) => response.json())
+    .then((response) => {
+      if (!response.ok) {
+        if (response.status === 404) {
+          return Notiflix.Notify.failure(
+            "Oops, there is no country with that name"
+          );
+        }
+        throw new Error(response.status);
+      }
+
+      return response.json();
+    })
     .catch((error) => console.log(error));
 };
